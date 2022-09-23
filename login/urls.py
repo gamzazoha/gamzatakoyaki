a7c72f17a2d603d6f2c73d1dac80b319904bfe3c
gamza <aprille138@gmail.com>
2022-09-23 18:44:22 +0900

    login_owner page 생성

--- login/urls.py
+++ login/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('home/', views.home, name='home'),
-
+    path('login_owner/', views.login, name='login_owner'),
     path('logout/', views.logout, name='logout'),
 ] 

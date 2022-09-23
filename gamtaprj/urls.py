cbbfd68fec4eccd25d1eaddf9e4df1579d8373f5
gamza <aprille138@gmail.com>
2022-09-23 16:23:50 +0900

    로그인 유알엘에스 연결

--- gamtaprj/urls.py
+++ gamtaprj/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 from home import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path(", views.login, name='login'),
-    path('home/', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('gamta/', views.index),
+    path('login/', include(login.urls))
 ]

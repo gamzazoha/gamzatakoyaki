c67b22decd615560b44529ae4dc8da83c048fccf
Hyeonho <hyeonhojo919@gmail.com>
2022-09-23 16:49:30 +0900

    made pages, syntax modified

--- login/views.py
+++ login/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def signup(request):
+    return HttpResponse("signup page")
+
+def login(request):
+    return HttpResponse("login page")
+
+def home(request):
+    return HttpResponse("home page")

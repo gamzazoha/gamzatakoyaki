d53b46e0aece77014c547c3e21d4af0c582f26ff
gamza <aprille138@gmail.com>
2022-09-24 14:40:23 +0900

    주은이를 위해

--- login/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class user(models.Model):
-    username = models.CharField(max_length=30, unique=True)
-    email = models.CharField(max_length=30, unique=True)
-    password = models.CharField(max_length=30)

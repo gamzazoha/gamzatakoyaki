c39a57f5994377cc8d8c24da17800bb63eb2a5f5
gamza <aprille138@gmail.com>
2022-09-24 14:29:42 +0900

    real login

--- login/urls.py
+++ login/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
     path('home/', views.home, name='home'),
     path('login_owner/', views.login, name='login_owner'),
     path('logout/', views.logout, name='logout'),
